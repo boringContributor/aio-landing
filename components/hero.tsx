d0d574b7fe86bbb6687ce61088b4bf46b8944d8b
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-04-30 00:04:48 +0200

    add animation delay

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,17 +28,26 @@ export const Hero = () => {
       <div className="mx-auto max-w-7xl px-6 pb-32  lg:px-8 ">
         <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
           <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-            <h1 className="text-center text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl animate-fade-up">
+            <h1 
+              className="text-center text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl animate-fade-up"
+              style={{ animationDelay: "0.1s", animationFillMode: "backwards" }}
+              >
                 All In One Gesundheitsmanagement
             </h1>
-            <p className="break-all srelative mt-6 leading-8 text-gray-600 sm:max-w-md lg:max-w-none animate-fade-up">
+            <p 
+              className="break-all srelative mt-6 leading-8 text-gray-600 sm:max-w-md lg:max-w-none animate-fade-up"
+              style={{ animationDelay: "0.25s", animationFillMode: "backwards" }}
+            >
               Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis cupidatat mollit aute velit. Et
               labore commodo nulla aliqua proident mollit ullamco exercitation tempor. Sint aliqua anim nulla sunt
               mollit id pariatur in voluptate cillum.
             </p>
           </div>
           <div className="mt-7 gap-8 sm:-mt-44 sm:pl-20 lg:mt-0 lg:pl-0">
-            <img src={'/team.jpg'} sizes="50vw" className="w-auto rounded-lg animate-fade-up" alt="Team Foto" />
+            <img 
+                src={'/team.jpg'} sizes="50vw" className="w-auto rounded-lg animate-fade-up delay-200" alt="Team Foto" 
+                style={{ animationDelay: "0.5s", animationFillMode: "backwards" }}
+            />
           </div>
         </div>
       </div>
