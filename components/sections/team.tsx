7ca172a27c5f0106fe2d6408b5e4692eb553612e
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-06-24 12:32:36 +0200

    refactor hero section

--- components/sections/team.tsx
+++ components/sections/team.tsx
@@ -24,7 +24,7 @@ const people = [
       <SimpleLayout id="team" title="Unser Team" >
           <ul
             role="list"
-            className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 place-items-center"
+            className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
           >
             {people.map((person) => (
               <li key={person.name} className='flex flex-col items-center'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { Team } from "../components/sections/team";
 export default function Page() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col gap-10">
-      <Hero/>
+      <Hero />
       <Team />
       <Portfolio />
       <Partner />
