1a94e4ea4b29f0881eb2cf5583d6f05d2bb6f000
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-06-25 12:19:56 +0200

    remove padding on hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div>
             <div className="text-base leading-7 text-gray-700 lg:max-w-lg">
